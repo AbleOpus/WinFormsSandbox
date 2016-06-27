152c7d468efad18a8859e9ba214c51d1ab7f5fa0
Brian Ferguson <brianferguson@hotmail.ca>
2016-06-27 01:22:21 -0300

    More intuitive test names and Assert methods used.

--- WinFormsSandbox/UnitTests.cs
+++ WinFormsSandbox/UnitTests.cs
@@ -17,7 +17,7 @@ namespace WinFormsSandbox
         /// Tests the GetPropertySignature method on properties having various modifiers.
         /// </summary>
         [TestMethod]
-        public void PropertySig_Assert()
+        public void ValidatePropertySig()
         {
             const BindingFlags FLAGS = BindingFlags.NonPublic | BindingFlags.Public
                 | BindingFlags.Static | BindingFlags.Instance;
@@ -26,111 +26,111 @@ namespace WinFormsSandbox
 
             PropertyInfo property = properties.First(p => p.Name == nameof(TestProp1));
             string val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "private System.Int32 TestProp1 { get; set; }", val);
+            Assert.AreEqual(val, "private System.Int32 TestProp1 { get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp2));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "protected System.Int32 TestProp2 { get; set; }", val);
+            Assert.AreEqual(val, "protected System.Int32 TestProp2 { get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp3));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "protected internal System.Int32 TestProp3 { get; set; }", val);
+            Assert.AreEqual(val, "protected internal System.Int32 TestProp3 { get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp4));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "internal System.Int32 TestProp4 { get; set; }", val);
+            Assert.AreEqual(val, "internal System.Int32 TestProp4 { get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp5));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "public System.Int32 TestProp5 { get; set; }", val);
+            Assert.AreEqual(val, "public System.Int32 TestProp5 { get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp6));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "public System.Int32 TestProp6 { private get; set; }", val);
+            Assert.AreEqual(val, "public System.Int32 TestProp6 { private get; set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp7));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "public System.Int32 TestProp7 { get; private set; }", val);
+            Assert.AreEqual(val, "public System.Int32 TestProp7 { get; private set; }", val);
 
             property = properties.First(p => p.Name == nameof(TestProp8));
             val = CSharpSourceGen.GetPropertySignature(property, true);
-            Assert.IsTrue(val == "public System.Int32 TestProp8 { get; protected internal set; }", val);
+            Assert.AreEqual(val, "public System.Int32 TestProp8 { get; protected internal set; }", val);
         }
 
         /// <summary>
         /// Tests the CSharpSourceGen.GetTypeName method with "fullyQualified" set to true.
         /// </summary>
         [TestMethod]
-        public void TypeNameFullyQualified_Assert()
+        public void ValidateFullTypeName()
         {
             string val = CSharpSourceGen.GetTypeName(typeof(MyClass5), true);
-            Assert.IsTrue(val == "WinFormsSandbox.UnitTests.MyClass5", val);
+            Assert.AreEqual(val, "WinFormsSandbox.UnitTests.MyClass5", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Action<Type, Type>), true);
-            Assert.IsTrue(val == "System.Action<System.Type, System.Type>", val);
+            Assert.AreEqual(val, "System.Action<System.Type, System.Type>", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Action<Type>), true);
-            Assert.IsTrue(val == "System.Action<System.Type>", val);
+            Assert.AreEqual(val, "System.Action<System.Type>", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Assert), true);
-            Assert.IsTrue(val == "Microsoft.VisualStudio.TestTools.UnitTesting.Assert", val);
+            Assert.AreEqual(val, "Microsoft.VisualStudio.TestTools.UnitTesting.Assert", val);
         }
 
         /// <summary>
         /// Tests the CSharpSourceGen.GetTypeName method with "fullyQualified" set to false.
         /// </summary>
         [TestMethod]
-        public void TypeName_Assert()
+        public void ValidateTypeName()
         {
             string val = CSharpSourceGen.GetTypeName(typeof(MyClass5), false);
-            Assert.IsTrue(val == "MyClass5", val);
+            Assert.AreEqual(val, "MyClass5", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Action<Type, Type>), false);
-            Assert.IsTrue(val == "Action<Type, Type>", val);
+            Assert.AreEqual(val, "Action<Type, Type>", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Action<Type>), false);
-            Assert.IsTrue(val == "Action<Type>", val);
+            Assert.AreEqual(val, "Action<Type>", val);
 
             val = CSharpSourceGen.GetTypeName(typeof(Assert), false);
-            Assert.IsTrue(val == "Assert", val);
+            Assert.AreEqual(val, "Assert", val);
         }
 
         /// <summary>
         /// Tests the GetTypeSignature method's ability to work with internal Types.
         /// </summary>
         [TestMethod]
-        public void TypeSigInternal_Assert()
+        public void ValidateInternalTypeSig()
         {
             // Namespace scope.
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass5), false);
-            Assert.IsTrue(val == "internal class MyClass5", val);
+            Assert.AreEqual(val, "internal class MyClass5", val);
             // Embedded.
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass1), false);
-            Assert.IsTrue(val == "internal class MyClass1", val);
+            Assert.AreEqual(val, "internal class MyClass1", val);
         }
 
         /// <summary>
         /// Tests the GetTypeSignature method's ability to work with private Types.
         /// </summary>
         [TestMethod]
-        public void TypeSigPrivate_Assert()
+        public void ValidatePrivateTypeSig()
         {
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass2), false);
-            Assert.IsTrue(val == "private class MyClass2", val);
+            Assert.AreEqual(val, "private class MyClass2", val);
         }
 
         /// <summary>
         /// Tests the GetTypeSignature method's ability to work with public Types.
         /// </summary>
         [TestMethod]
-        public void TypeSigPublic_Assert()
+        public void ValidatePublicTypeSig()
         {
             // Namespace scope.
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass6), false);
-            Assert.IsTrue(val == "public class MyClass6", val);
+            Assert.AreEqual(val, "public class MyClass6", val);
             // Embedded.
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass3), false);
-            Assert.IsTrue(val == "public class MyClass3", val);
+            Assert.AreEqual(val, "public class MyClass3", val);
         }
 
         /// <summary>
@@ -138,14 +138,14 @@ namespace WinFormsSandbox
         /// or parameters.
         /// </summary>
         [TestMethod]
-        public void TypeSigGenerics_Assert()
+        public void ValidateGenericTypeSig()
         {
             // Embedded scope, two type params.
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass4<string, string>), false);
-            Assert.IsTrue(val == "internal class MyClass4<string, string>", val);
+            Assert.AreEqual(val, "internal class MyClass4<string, string>", val);
             // Namespace scope, one type param.
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass7<Type>), false);
-            Assert.IsTrue(val == "internal class MyClass7<Type>", val);
+            Assert.AreEqual(val, "internal class MyClass7<Type>", val);
         }
 
         /// <summary>
@@ -153,42 +153,42 @@ namespace WinFormsSandbox
         /// or parameters.
         /// </summary>
         [TestMethod]
-        public void TypeSigSealedAndAbstract_Assert()
+        public void ValidateSealedAndAbstractTypeSig()
         {
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass8), false);
-            Assert.IsTrue(val == "public sealed class MyClass8", val);
+            Assert.AreEqual(val, "public sealed class MyClass8", val);
 
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass9), false);
-            Assert.IsTrue(val == "public abstract class MyClass9", val);
+            Assert.AreEqual(val, "public abstract class MyClass9", val);
         }
 
         /// <summary>
         /// Tests the GetTypeSignature method's ability to work with value Types.
         /// </summary>
         [TestMethod]
-        public void TypeSigStruct_Assert()
+        public void ValidateStructTypeSig()
         {
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyStruct1), false);
-            Assert.IsTrue(val == "internal struct MyStruct1", val);
+            Assert.AreEqual(val, "internal struct MyStruct1", val);
 
             val = CSharpSourceGen.GetTypeSignature(typeof(MyStruct2), false);
-            Assert.IsTrue(val == "public struct MyStruct2", val);
+            Assert.AreEqual(val, "public struct MyStruct2", val);
         }
 
         /// <summary>
         /// Tests the GetTypeSignature method's ability to work with derived Types.
         /// </summary>
         [TestMethod]
-        public void TypeSigDeriving_Assert()
+        public void ValidateDerivingTypeSig()
         {
             string val = CSharpSourceGen.GetTypeSignature(typeof(MyClass11), false);
-            Assert.IsTrue(val == "public class MyClass11 : MyClass9", val);
+            Assert.AreEqual(val, "public class MyClass11 : MyClass9", val);
 
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass11), true);
-            Assert.IsTrue(val == "public class MyClass11 : WinFormsSandbox.UnitTests.MyClass9", val);
+            Assert.AreEqual(val, "public class MyClass11 : WinFormsSandbox.UnitTests.MyClass9", val);
 
             val = CSharpSourceGen.GetTypeSignature(typeof(MyClass12), false);
-            Assert.IsTrue(val == "internal class MyClass12 : MyClass7<string>", val);
+            Assert.AreEqual(val, "internal class MyClass12 : MyClass7<string>", val);
         }
 
 
@@ -196,79 +196,79 @@ namespace WinFormsSandbox
         /// Tests the GetMethodSignature method's ability to work with static methods.
         /// </summary>
         [TestMethod]
-        public void StaticMethodSig_Assert()
+        public void ValidateStaticTypeSig()
         {
             Type dummyMethodsType = typeof(DummyMethods);
             const BindingFlags STATIC = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
 
             var method = dummyMethodsType.GetMethod("TestMethodPrivateStatic1", STATIC);
             var val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "private static void TestMethodPrivateStatic1()", val);
+            Assert.AreEqual(val, "private static void TestMethodPrivateStatic1()", val);
 
             method = dummyMethodsType.GetMethod("TestMethodPublicStatic2", STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "public static void TestMethodPublicStatic2()", val);
+            Assert.AreEqual(val, "public static void TestMethodPublicStatic2()", val);
 
             method = dummyMethodsType.GetMethod("TestMethodInternalStatic3", STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "internal static void TestMethodInternalStatic3()", val);
+            Assert.AreEqual(val, "internal static void TestMethodInternalStatic3()", val);
 
             method = dummyMethodsType.GetMethod("TestMethodProtectedStatic4", STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected static void TestMethodProtectedStatic4()", val);
+            Assert.AreEqual(val, "protected static void TestMethodProtectedStatic4()", val);
 
             method = dummyMethodsType.GetMethod("TestMethodProtectedInternalStatic5", STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected internal static void TestMethodProtectedInternalStatic5()", val);
+            Assert.AreEqual(val, "protected internal static void TestMethodProtectedInternalStatic5()", val);
         }
 
         /// <summary>
         /// Tests the GetMethodSignature method's ability to work with instance methods.
         /// </summary>
         [TestMethod]
-        public void InstanceMethodSig_Assert()
+        public void ValidateInstanceMethodSig()
         {
             const BindingFlags NON_STATIC = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
             Type dummyMethodsType = typeof(DummyMethods);
             // Private
             var method = dummyMethodsType.GetMethod("TestMethodPrivate6", NON_STATIC);
             var val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "private void TestMethodPrivate6()", val);
+            Assert.AreEqual(val, "private void TestMethodPrivate6()", val);
             // Public
             method = dummyMethodsType.GetMethod("TestMethodPublic7", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "public void TestMethodPublic7()", val);
+            Assert.AreEqual(val, "public void TestMethodPublic7()", val);
             // Internal
             method = dummyMethodsType.GetMethod("TestMethodInternal8", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "internal void TestMethodInternal8()", val);
+            Assert.AreEqual(val, "internal void TestMethodInternal8()", val);
             // Protected
             method = dummyMethodsType.GetMethod("TestMethodProtected9", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected void TestMethodProtected9()", val);
+            Assert.AreEqual(val, "protected void TestMethodProtected9()", val);
             // protected Internal
             method = dummyMethodsType.GetMethod("TestMethodProtectedInternal10", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected internal void TestMethodProtectedInternal10()", val);
+            Assert.AreEqual(val, "protected internal void TestMethodProtectedInternal10()", val);
             // protected Virtual
             method = dummyMethodsType.GetMethod("TestMethodProtectedVirtual11", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected virtual void TestMethodProtectedVirtual11()", val);
+            Assert.AreEqual(val, "protected virtual void TestMethodProtectedVirtual11()", val);
             // protected Abstract
             method = typeof(DummyMethods2).GetMethod("TestMethodProtectedAbstract12", NON_STATIC);
             val = CSharpSourceGen.GetMethodSignature(method, false);
-            Assert.IsTrue(val == "protected abstract void TestMethodProtectedAbstract12()", val);
+            Assert.AreEqual(val, "protected abstract void TestMethodProtectedAbstract12()", val);
         }
 
         /// <summary>
         /// Tests GetStatic extension method.
         /// </summary>
         [TestMethod]
-        public void StaticType_Assert()
+        public void IsStaticReturnCheck()
         {
             Assert.IsTrue(typeof(MyClass10).IsStatic());
-            Assert.IsTrue(!typeof(MyClass9).IsStatic());
-            Assert.IsTrue(!typeof(MyClass5).IsStatic());
+            Assert.IsFalse(typeof(MyClass9).IsStatic());
+            Assert.IsFalse(typeof(MyClass5).IsStatic());
         }
 
 #pragma warning disable 1591
